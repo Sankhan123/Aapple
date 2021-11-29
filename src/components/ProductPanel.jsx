2b0e3e3d8932503bec3e5d836101a85683f1cfea
Sharan AJ <sharanajs2001@gmail.com>
2021-11-29 18:27:23 +0530

    product panel build

--- src/components/ProductPanel.jsx
+++ src/components/ProductPanel.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 
 const ProductPanel = () => {
-    return <p>Product Panel</p>;
+    return <section className="container">
+        <h1 className="alert alert-info mb-5 p-2">Product Panel</h1>
+        <table className="table">
+    <thead className="table-dark">
+      <tr>
+        <th scope="col">Product Name</th>
+        <th scope="col">Serial No</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Mark</td>
+        <td>1239</td>
+      </tr>
+      <tr>
+        <td>Jacob</td>
+        <td>1111</td>
+      </tr>
+      <tr>
+        <td>Larry the Bird</td>
+        <td>9999</td>
+      </tr>
+    </tbody>
+  </table>
+  </section>
 }
 export default ProductPanel;
