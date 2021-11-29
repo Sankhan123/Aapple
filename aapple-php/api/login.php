4ae061c05e94bb116ae211c82cf8cf2f439dbda2
SANTHOSH <kanism33@gmail.com>
2021-11-29 16:12:47 +0530

    admin approval checked

--- aapple-php/api/login.php
+++ aapple-php/api/login.php
@@ -14,7 +14,7 @@ $data = json_decode(file_get_contents("php://input"));
     $password = $data->password;
     
 
-    $sql = "SELECT * FROM `user_register` WHERE `email`='$email' and `password`='$password' and `user_status`='true'";
+    $sql = "SELECT * FROM `user_register` WHERE `email`='$email' and `password`='$password'";
 
     $smt = $conn->query($sql);
 
@@ -23,10 +23,26 @@ $data = json_decode(file_get_contents("php://input"));
     $response = array();
 
     if($count>0){
-        $response = array(
-            'status' => 'ok',
-            'message' => 'Login Success'
-        );
+
+        $sql1 = "SELECT * FROM `user_register` WHERE `email`='$email' and `password`='$password' and `user_status`='true'";
+
+        $smt1 = $conn->query($sql1);
+
+        $count1 = $smt1->rowCount();
+
+        if($count1>0){
+            $response = array(
+                'status' => 'ok',
+                'message' => 'Login Success'
+            );
+        }
+        else{
+            $response = array(
+                'status' => 'fail',
+                'message' => "You can't login.! Please wait for admin approval, Thank you..!"
+            );
+        }
+        
     }else{
         $response = array(
             'status' => 'fail',
