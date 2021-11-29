9fd52cfc2ed81de96f5bd3bd70f1a749c51e8efb
SANTHOSH <kanism33@gmail.com>
2021-11-29 18:29:34 +0530

    db update and product list added

--- aapple-php/api/database/aapple_paints db.sql
+++ aapple-php/api/database/aapple_paints db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2021 at 12:36 PM
+-- Generation Time: Nov 29, 2021 at 01:23 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -28,17 +28,25 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `products` (
-  `premium synthetic enamel` varchar(100) NOT NULL,
-  `hi-gloss automotive paint(fast drying)` varchar(100) NOT NULL,
-  `aapple wood primers` varchar(100) NOT NULL,
-  `aapple hamertones & aluminium paints` varchar(100) NOT NULL,
-  `aapple metal primers` varchar(100) NOT NULL,
-  `aapple furniture enamels` varchar(100) NOT NULL,
-  `galaxy truck coating paints` varchar(100) NOT NULL,
-  `multi wall primer water base` varchar(100) NOT NULL,
-  `floor coat` varchar(100) NOT NULL,
-  `cool roof cote` varchar(100) NOT NULL
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`) VALUES
+(1, 'Premium synthetic enamel'),
+(2, 'Hi-gloss automotive paint(fast drying)'),
+(3, 'Aapple wood primers'),
+(4, 'Aapple hamertones & aluminium paints'),
+(5, 'Aapple metal primers'),
+(6, 'Aapple furniture enamels'),
+(7, 'Galaxy truck coating paints'),
+(8, 'Multi wall primer water base'),
+(9, 'Floor coat'),
+(10, 'Cool roof coat');
 
 -- --------------------------------------------------------
 
@@ -69,22 +77,19 @@ CREATE TABLE `user_register` (
 --
 
 INSERT INTO `user_register` (`reg_id`, `email`, `password`, `company_name`, `gst_number`, `contact_person`, `address`, `city`, `district`, `state`, `zip`, `phone`, `alternate_number`, `user_status`, `user_role`) VALUES
-(1, 'san@gmail.com', 'san', 'as', 'as', 'ds', 'da', 'eew', 'w', 'e', 'f', '21', '23', 'true', 'admin'),
-(7, 'ravi@gmail.com', 'ravi', 'dsd', 'sd', 'sds', 'sd', 'sd', 'sds', 'sds', 'sds', 'sds', 'sa', 'false', 'user'),
-(8, 'pavisakthi1699@gmail.com', 'pavi', 's', '454545465454', 'pavithra', 'tiruchengode', 'tiruchengode', 'namakkal', 'Tamil Nadu', '637211', '+918428716864', '+918428716864', 'false', 'user'),
-(9, 'pavisakthi1699@gmail.com', 'ddgd', 's', '68765767979', 'pavithra', 'tiruchengode', 'tiruchengode', 'ohqhduqhd', 'Tamil Nadu', '637211', '+918428716864', '+918428716864', 'false', 'user'),
-(10, 'santhosh@gmail.com', 'santhosh', '', '', '', 'sd', '', '', '', '', '', '', 'false', 'user'),
-(11, 'santhosh2@gmail.com', 'santhosh', '', '', '', 'sd', 'sd', 'sds', 'sdsd', 'sdsd', '', '', 'false', 'user'),
-(12, 'sss@gmail.com', 'sdsdsd', '', '', '', 'sdsd', 'sdsd', 'sdsd', 'ssds', '', '', '', 'false', 'user'),
-(13, 'santhosh@a.d', 'sdsdsdsd', '', '', '', 'sdsd', '', '', '', '', '', '', 'false', 'user'),
-(14, 'sa@gmail.com', 'dsdssdsd', '', '', '', '', '', '', '', '', '', '', 'false', 'user'),
-(15, 'asas@jkjs.as', 'asadsda', '', 'asas', 'asa', '', '', '', '', '', 'wew', '', 'false', 'user'),
-(16, 'as@ahsa.kjh', 'hjhjjhj', '', 'hjhj', 'hjhjh', '', '', '', '', '', 'jhj', '', 'false', 'user'),
-(17, 'ssss@gmail.com', 'sssssd', '', '22sww', 'sds', '', '', '', '', '', '23232', '', 'false', 'user');
+(1, 'san@gmail.com', '123456', 'as', 'as', 'ds', 'da', 'eew', 'w', 'e', 'f', '21', '23', 'true', 'admin'),
+(7, 'ravi@gmail.com', 'ravi123', 'dsd', 'sd', 'sds', 'sd', 'sd', 'sds', 'sds', 'sds', 'sds', 'sa', 'false', 'user'),
+(8, 'pavisakthi1699@gmail.com', 'pavi123', 's', '454545465454', 'pavithra', 'tiruchengode', 'tiruchengode', 'namakkal', 'Tamil Nadu', '637211', '+918428716864', '+918428716864', 'false', 'user');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
 
 --
 -- Indexes for table `user_register`
@@ -95,6 +100,12 @@ ALTER TABLE `user_register`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user_register`
