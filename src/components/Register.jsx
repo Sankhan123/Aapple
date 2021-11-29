9d89dbef84997c953f6f2438e62a9b85078d36ac
Sharan AJ <sharanajs2001@gmail.com>
2021-11-29 15:25:43 +0530

    Register component reworked

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -32,6 +32,13 @@ const Register = () => {
           contactPerson: "",
           gstNumber: "",
           contactNumber: "",
+          companyName: "",
+          address: "",
+          district: "",
+          city: "",
+          state: "",
+          zip: "",
+          alternateNumber: "",
         }}
         validationSchema={SignupSchema}
         onSubmit={(values) => {
