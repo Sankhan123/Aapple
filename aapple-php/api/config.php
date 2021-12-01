980b61dc402c144dfe787090f89a29919739fe27
SANTHOSH <kanism33@gmail.com>
2021-12-01 21:42:29 +0530

    config file db name edited

--- aapple-php/api/config.php
+++ aapple-php/api/config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "aapple_paint";
+$dbname = "aapple_paints";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
