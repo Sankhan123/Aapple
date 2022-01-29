3199cff65933bf1ac567e610206117d89f5b1556
Sharan AJ <sharanajs2001@gmail.com>
2022-01-29 22:15:32 +0530

    order details completed

--- src/screens/user/Purchase.jsx
+++ src/screens/user/Purchase.jsx
@@ -8,10 +8,9 @@ function Purchase() {
   const [purchaseData, setProducts] = useState(null);
   const [cartData, setData] = useState([]);
   const [total, setTotal] = useState(0);
+  const [count, setCount] = useState(0);
 
-  
   useEffect(() => {
-    
     async function getproducts() {
       try {
         const res = await axios.get(`${REACT_APP_API_URL}/get-products`, {
@@ -28,40 +27,56 @@ function Purchase() {
     getproducts();
   }, []);
 
-  async function addOrder(){
-    let dealer_id='';
-        if(sessionStorage.length){
-            const dealer_val = sessionStorage.getItem('user');
-            const dealer = JSON.parse(dealer_val);
-            dealer_id = dealer.user.reg_id; 
-        }
-    let data = {
-      dealer_id : dealer_id,
-      order : cartData,
-      pro_count : total,
-      order_status : "Pending",
+  async function addOrder() {
+    let dealer_id = "";
+    if (sessionStorage.length) {
+      const dealer_val = sessionStorage.getItem("user");
+      const dealer = JSON.parse(dealer_val);
+      dealer_id = dealer.user.reg_id;
     }
-    try{
-      const response = await axios.post(`${REACT_APP_API_URL}/add-order`,data);
-      if(response){
+    let data = {
+      dealer_id: dealer_id,
+      order: cartData,
+      pro_count: total,
+      order_status: "Pending",
+    };
+    try {
+      const response = await axios.post(`${REACT_APP_API_URL}/add-order`, data);
+      if (response) {
         alert("Your order created successfully");
       }
-    }catch(e){
-      console.log(e)
+    } catch (e) {
+      console.log(e);
     }
   }
+  console.log(cartData);
+  console.log(purchaseData);
   return (
     <>
-      {purchaseData && purchaseData.map((purchaseData)=>(
+      {purchaseData && (
         <PurchaseTable
-          catagoryName={purchaseData.cat_name}
-          data={purchaseData}
+          catagoryName={purchaseData[count].cat_name}
+          data={purchaseData[count]}
           cartData={cartData}
           setData={setData}
           setTotal={setTotal}
         />
-        ))}
+      )}
       <div className="alert alert-secondary text-end">
+        <button
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          left
+        </button>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          right
+        </button>
         <button onClick={addOrder}>Submit</button>
         <span>Total Product : </span>
         <span>
@@ -81,17 +96,25 @@ function PurchaseTable({ catagoryName, data, cartData, setData, setTotal }) {
       });
       setTotal(total);
     }
-  }, [cartData,setTotal]);
-  function handleChange(e, productId, sizeId, catId,productName,sizeName,catagoryName) {
+  }, [cartData, setTotal]);
+  function handleChange(
+    e,
+    productId,
+    sizeId,
+    catId,
+    productName,
+    sizeName,
+    catagoryName
+  ) {
     let data = JSON.parse(JSON.stringify(cartData));
     if (data.length === 0) {
       data.push({
         cat_id: catId,
-        cat_name:catagoryName,
+        cat_name: catagoryName,
         size_id: sizeId,
-        size_name:sizeName,
+        size_name: sizeName,
         product_id: productId,
-        product_name:productName,
+        product_name: productName,
         value: parseInt(e.target.value),
       });
     } else {
@@ -109,27 +132,49 @@ function PurchaseTable({ catagoryName, data, cartData, setData, setTotal }) {
       if (rowIndex === -1) {
         data.push({
           cat_id: catId,
-        cat_name:catagoryName,
-        size_id: sizeId,
-        size_name:sizeName,
-        product_id: productId,
-        product_name:productName,
-        value: parseInt(e.target.value),
+          cat_name: catagoryName,
+          size_id: sizeId,
+          size_name: sizeName,
+          product_id: productId,
+          product_name: productName,
+          value: parseInt(e.target.value),
         });
       } else {
         data[rowIndex] = {
           cat_id: catId,
-        cat_name:catagoryName,
-        size_id: sizeId,
-        size_name:sizeName,
-        product_id: productId,
-        product_name:productName,
-        value: parseInt(e.target.value),
+          cat_name: catagoryName,
+          size_id: sizeId,
+          size_name: sizeName,
+          product_id: productId,
+          product_name: productName,
+          value: parseInt(e.target.value),
         };
       }
     }
     setData(data);
   }
+
+  const getValues = (product_id, size_id, cat_id) => {
+    console.log(product_id, size_id, cat_id, cartData, "deivaraj");
+    if (cartData.length > 0) {
+      let rowIndex = cartData.findIndex((rowData) => {
+        if (
+          rowData.cat_id === cat_id &&
+          rowData.size_id === size_id &&
+          rowData.product_id === product_id
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      if (rowIndex !== -1) {
+        return cartData[rowIndex].value;
+      } else {
+        return "";
+      }
+    }
+  };
   return (
     <>
       <section className="col">
@@ -162,8 +207,17 @@ function PurchaseTable({ catagoryName, data, cartData, setData, setTotal }) {
                               min="0"
                               max="100"
                               onChange={(e) => {
-                                handleChange(e, pro.id, size.id, pro.cat_id,pro.product_name,size.size_name,catagoryName);
+                                handleChange(
+                                  e,
+                                  pro.id,
+                                  size.id,
+                                  pro.cat_id,
+                                  pro.product_name,
+                                  size.size_name,
+                                  catagoryName
+                                );
                               }}
+                              value={getValues(pro.id, size.id, pro.cat_id)}
                             />
                           </td>
                         );
