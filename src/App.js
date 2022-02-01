280f49f9c2eb1bf420503ed4d04b1c0a72eb463a
ravikumar <ravikumarwolfd9130@gmail.com>
2022-02-01 10:38:39 +0530

    ~ auth controlled

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ import {
 } from "./screens/user/userRoutes";
 import setTitle from "./assets/header/setTitle";
 
+import PrivateRoute from './assets/header/PrivateRoute'
+import ProtectedRoute from './assets/header/ProductedRoute'
+
 function App() {
   let location = useLocation();
   useEffect(() => {
@@ -29,35 +32,35 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/user-dashboard/order/data" element={<Tabel />} />
-        <Route path="/user-dashboard/pending" element={<Process />} />
-        <Route path="/user-dashboard/complete/cdata" element={<Complete />} />
+        <Route path="/user-dashboard/order/data" element={<PrivateRoute><Tabel /></PrivateRoute>} />
+        <Route path="/user-dashboard/pending" element={<PrivateRoute><Process /></PrivateRoute>} />
+        <Route path="/user-dashboard/complete/cdata" element={<PrivateRoute><Complete /></PrivateRoute>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />}>
-          <Route path="dealer-request" element={<DealerRequest />} />
-          <Route path="transaction" element={<TransactionDetail />} />
-          <Route path="all-dealers" element={<AllDealers />}>
-            <Route path=":id" element={<DealerDetails />} />
+        <Route path="/admin-dashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>}>
+          <Route path="dealer-request" element={<ProtectedRoute><DealerRequest /></ProtectedRoute>} />
+          <Route path="transaction" element={<ProtectedRoute></ProtectedRoute>} />
+          <Route path="all-dealers" element={<ProtectedRoute><AllDealers /></ProtectedRoute>}>
+            <Route path=":id" element={<ProtectedRoute><DealerDetails /></ProtectedRoute>} />
           </Route>
           
-          <Route path="new-orders" element={<NewOrders />} >
-          <Route path=":id" element={<OrderDetails />} />
+          <Route path="new-orders" element={<ProtectedRoute><NewOrders /></ProtectedRoute>} >
+          <Route path=":id" element={<ProtectedRoute><OrderDetails /></ProtectedRoute>} />
           
          
           
             </Route>
-          <Route path="product-panel" element={<ProductPanel />} />
+          <Route path="product-panel" element={<ProtectedRoute><ProductPanel /></ProtectedRoute>} />
         </Route>
-        <Route path="/user-dashboard" element={<UserDashboard />}>
-          <Route path="purchase" element={<Purchase />} />
-          <Route path="transaction" element={<Transaction />} />
-          <Route path="order" element={<Uorder />} >
+        <Route path="/user-dashboard" element={<PrivateRoute><UserDashboard /></PrivateRoute>}>
+          <Route path="purchase" element={<PrivateRoute><Purchase /></PrivateRoute>} />
+          <Route path="transaction" element={<PrivateRoute><Transaction /></PrivateRoute>} />
+          <Route path="order" element={<PrivateRoute><Uorder /></PrivateRoute>} >
          
           </Route>
          
-          <Route path="process" element={<Uprocessing />} />
-          <Route path="complete" element={<Ucomplete />} />
+          <Route path="process" element={<PrivateRoute><Uprocessing /></PrivateRoute>} />
+          <Route path="complete" element={<PrivateRoute><Ucomplete /></PrivateRoute>} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
