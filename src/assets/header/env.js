d4c82ce52335d49eb3c5b06c0458195c06ad5eb3
SANTHOSH <kanism33@gmail.com>
2022-03-01 16:05:02 +0530

    hello

--- src/assets/header/env.js
+++ src/assets/header/env.js
@@ -1,3 +1,3 @@
-//const REACT_APP_API_URL = 'https://ragavarubberspares.com/aapple-api/api';
-const REACT_APP_API_URL = "http://127.0.0.1:8000/api";
+const REACT_APP_API_URL = 'https://aapplepaints.ragavarubberspares.com/aapple-api/api';
+//const REACT_APP_API_URL = "http://127.0.0.1:8000/api";
 export default REACT_APP_API_URL;
